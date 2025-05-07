6ae9e6f37c2b6385d02649503da1a0854b3c2d58
GabrielC67 <gabecruzpro@gmail.com>
2025-05-07 17:10:56 -0400

    Error addressed

--- signin.js
+++ signin.js
@@ -26,7 +26,7 @@ form.addEventListener('submit', function (e) {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json'
-                }
+                },
                 body: JSON.stringify(user)
             })
                 .then(res => {
